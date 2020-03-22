d8c1bc70241babd32cfb25f3b8b99079fecb9d56
ace <allisterg@gmail.com>
2020-03-21 23:42:53 -0400

    remove merge conflict message

--- src/store/reducers/authorsReducer.js
+++ src/store/reducers/authorsReducer.js
@@ -1,12 +1,6 @@
 
 import { combineReducers } from 'redux';
 
-<<<<<<< HEAD
-const reducer = combineReducers({
-=======
 export default combineReducers({
->>>>>>> master
     // insert your reducer here!
 });
-
-export default reducer;
