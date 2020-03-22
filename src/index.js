40e12cd2b4ad0bc49ee2707f9a0a78e04060a761
ace <allisterg@gmail.com>
2020-03-21 23:54:40 -0400

    remove Provider from index.js as it is in app.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import App from './App';
-import { Provider } from 'react-redux'
-
 import * as serviceWorker from './serviceWorker';
 
-import store from './store';
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
+ <App />
+   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
