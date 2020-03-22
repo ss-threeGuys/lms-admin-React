42317e7f827d1ce35e4b8462e28c7f0e085c9b04
ace <allisterg@gmail.com>
2020-03-21 23:45:57 -0400

    remove comment from commented reducers

--- src/store/reducers/branchesReducer.js
+++ src/store/reducers/branchesReducer.js
@@ -3,4 +3,3 @@ import { combineReducers } from 'redux';
 export default combineReducers({
     // insert your reducer here!
 });
-
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 
-// import authorsReducer from './authorsReducer';
+import authorsReducer from './authorsReducer';
 import booksReducer from './booksReducer';
-// import genresReducer from './genresReducer';
-// import publishersReducer from './publishersReducer';
-// import branchesReducer from './branchesReducer';
-// import borrowersReducer from './borrowersReducer';
+import genresReducer from './genresReducer';
+import publishersReducer from './publishersReducer';
+import branchesReducer from './branchesReducer';
+import borrowersReducer from './borrowersReducer';
 
 
 
@@ -26,10 +26,10 @@ export const Task = {
 }
 
 export default combineReducers({
-    // [Target.AUTHOR]     : authorsReducer,
+    [Target.AUTHOR]     : authorsReducer,
     booksReducer,
-    // [Target.GENRE]      : genresReducer,
-    // [Target.PUBLISHER]  : publishersReducer,
-    // [Target.BRANCH]     : branchesReducer,
-    // [Target.BORROWER]   : borrowersReducer,
+    [Target.GENRE]      : genresReducer,
+    [Target.PUBLISHER]  : publishersReducer,
+    [Target.BRANCH]     : branchesReducer,
+    [Target.BORROWER]   : borrowersReducer,
 });
