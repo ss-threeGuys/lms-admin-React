ee67e414884981f4df7ee9adc5c76699cb786224
ace <allisterg@gmail.com>
2020-03-22 20:20:56 -0400

    move Provider back to index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store'
-
 import "./App.css";
-
 import Header from './containers/Header'
 import AppBody from './containers/AppBody'
 
@@ -12,12 +8,11 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <HashRouter>
-          <Header />
-          <AppBody />
-        </HashRouter>
-      </Provider>
+      <HashRouter>
+        <Header />
+        <AppBody />
+      </HashRouter>
+
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import store from './store'
+import { Provider } from 'react-redux';
 
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -10,8 +12,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
- <App />
-   , document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
