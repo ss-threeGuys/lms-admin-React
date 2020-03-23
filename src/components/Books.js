034ba660fea32428400188448b9ee8da9b5c6937
ace <allisterg@gmail.com>
2020-03-22 23:12:29 -0400

    add books render component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { readBooks, addBooks, updateBooks, deleteBooks, readAllAuthors, readAllGenres, readAllPublishers } from '../store/actions/bookActions'
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
-import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
-import { MultiSelect } from 'primereact/multiselect';
-import { Dropdown } from 'primereact/dropdown'
+import { BooksRender } from './BooksRender';
 import { Target } from '../store/reducers';
-
 export class Books extends React.Component {
-
 
     constructor(props) {
         super(props);
@@ -21,16 +13,12 @@ export class Books extends React.Component {
             displayDialog: false,
             valid: false,
         }
-
-
-
     }
 
     getAllAuthors() {
         readAllAuthors()
             .then(_authors => {
                 this.setState({ authors: _authors.data.map(author => ({ label: author.name, value: author._id })) })
-
             })
     }
 
@@ -49,16 +37,11 @@ export class Books extends React.Component {
     }
 
     componentDidMount() {
-        console.log('in Component Did Mount')
-
         this.props.dispatch(readBooks(this.props.sortField, this.props.sortOrder, this.props.currentPage, this.props.pageSize));
         this.getAllPublishers();
         this.getAllGenres();
         this.getAllAuthors();
-
     }
-
-
 
     onBookSelect = (e) => {
         console.log(e.data);
@@ -79,11 +62,9 @@ export class Books extends React.Component {
     }
 
     onPage = (event) => {
-
         const currentPage = 1 + (event.first / this.props.pageSize)
         this.props.dispatch(readBooks(this.props.sortField, this.props.sortOrder, currentPage, this.props.pageSize))
         this.setState({ first: event.first })
-
     }
 
     onSort = (event) => {
@@ -97,18 +78,13 @@ export class Books extends React.Component {
         let sortField = sortFields[event.sortField]
         let sortOrder = this.props.sortOrder * -1;
 
-
         this.props.dispatch(readBooks(sortField, sortOrder, this.props.currentPage, this.props.pageSize))
 
         this.setState({
             sortField: sortField,
             sortOrder: sortOrder
         })
-
-
-
     }
-
 
     addNew = () => {
         this.newBook = true;
@@ -120,7 +96,7 @@ export class Books extends React.Component {
     }
 
     save = () => {
-    
+
         if (this.newBook) {
             this.props.dispatch(addBooks(this.state.book))
                 .then(() => {
@@ -134,7 +110,6 @@ export class Books extends React.Component {
                 })
         }
 
-
         this.setState({ selectedBook: null, book: null, displayDialog: false });
     }
 
@@ -147,8 +122,6 @@ export class Books extends React.Component {
             displayDialog: false
         });
     }
-
-
 
     updateProperty = (property, value) => {
         let book = this.state.book;
@@ -178,63 +151,34 @@ export class Books extends React.Component {
 
     render() {
 
-        const outputBooks = this.mapBooksToOutputBook(this.props.books);
-       
-        let footer = <div className="p-clearfix" style={{ width: '100%' }}>
-            <Button style={{ float: 'left' }} label="Add" icon="pi pi-plus" onClick={this.addNew} />
-        </div>;
-
-        let dialogFooter = <div className="ui-dialog-buttonpane p-clearfix">
-            {(!this.newBook) && <Button label="Delete" icon="pi pi-times" onClick={this.delete} />}
-            <Button disabled={!this.state.valid} label="Save" icon="pi pi-check" onClick={this.save} />
-        </div>;
-
-        if (this.props.loading) {
-            return <p>loading...</p>
-        }
-
-
         return (
-
-
             <div>
-                {this.componentName}
-                <DataTable value={outputBooks} paginator={true} rows={this.props.pageSize} totalRecords={this.props.count}
-                    lazy={true} first={this.state.first} onPage={this.onPage} onSort={this.onSort} loading={this.props.loading} footer={footer}
-                    selectionMode="single" selection={this.state.selectedBook} onSelectionChange={e => this.setState({ selectedBook: e.value })}
-                    onRowSelect={this.onBookSelect}>
-
-                    <Column field="title" header="Title" sortable={true} />
-                    <Column field="authorNames" header="Authors" sortable={true} />
-                    <Column field="genreNames" header="Genres" sortable={true} />
-                    <Column field="publisherName" header="Publisher" sortable={true} />
-                </DataTable>
-
-                <Dialog visible={this.state.displayDialog} width="300px" header="Book Details" modal={true} footer={dialogFooter} onHide={() => this.setState({ displayDialog: false })}>
-                    {
-                        this.state.book &&
-
-                        <div className="p-grid p-fluid">
-                            <div className="p-col-4" style={{ padding: '.75em' }}><label htmlFor="title">Title</label></div>
-                            <div className="p-col-8" style={{ padding: '.5em' }}>
-                                <InputText id="title" required={true} onChange={(e) => { this.updateProperty('title', e.target.value) }} value={this.state.book.title} />
-                            </div>
-                            <div className="p-col-4" style={{ padding: '.75em' }}><label htmlFor="authors">Authors</label></div>
-                            <div className="p-col-8" style={{ padding: '.5em' }}>
-                                <MultiSelect options={this.state.authors} id="authors" onChange={(e) => { this.updateProperty('authors', e.target.value) }} value={this.state.book.authors} filter={true} />
-                            </div>
-                            <div className="p-col-4" style={{ padding: '.75em' }}><label htmlFor="genres">Genres</label></div>
-                            <div className="p-col-8" style={{ padding: '.5em' }}>
-                                <MultiSelect options={this.state.genres} id="genres" onChange={(e) => { this.updateProperty('genres', e.target.value) }} value={this.state.book.genres} filter={true} />
-                            </div>
-                            <div className="p-col-4" style={{ padding: '.75em' }}><label htmlFor="publishers">Publishers</label></div>
-                            <div className="p-col-8" style={{ padding: '.5em' }}>
-                                <Dropdown options={this.state.publishers} id="publishers" onChange={(e) => { this.updateProperty('publisher', e.target.value) }} value={this.state.book.publisher} filter={true} />
-                            </div>
-                        </div>
-                    }
-
-                </Dialog>
+                <BooksRender
+                    mapBooksToOutputBook={this.mapBooksToOutputBook}
+                    books={this.props.books}
+                    addNew={this.addNew}
+                    delete={this.delete}
+                    valid={this.state.valid}
+                    save={this.save}
+                    loading={this.props.loading}
+                    componentName={this.componentName}
+                    pageSize={this.props.pageSize}
+                    totalRecords={this.props.count}
+                    first={this.state.first}
+                    onPage={this.onPage}
+                    onSort={this.onSort}
+                    selectedBook={this.state.selectedBook}
+                    onSelectionChange={e => this.setState({ selectedBook: e.value })}
+                    onRowSelect={this.onBookSelect}
+                    displayDialog={this.state.displayDialog}
+                    onHide={() => this.setState({ displayDialog: false })}
+                    book={this.state.book}
+                    onChange={(e, property) => { this.updateProperty(property, e.target.value) }}
+                    publishers={this.state.publishers}
+                    authors={this.state.authors}
+                    genres={this.state.genres}
+                    newBook={this.newBook}
+                /> 
             </div>
 
         );
