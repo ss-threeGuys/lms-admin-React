9443beb41859ef324bd64af800b605c891c31591
Saurav <sbhuju61@gmail.com>
2020-03-25 14:01:17 -0400

    configure enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  render(<App />, div);
+  shallow(<App />);
 });
